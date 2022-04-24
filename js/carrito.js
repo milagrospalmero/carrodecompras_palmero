74f1a83d08f0238fe9696a0cc95a0c4502bc5993
milagros palmero <milagrospalmero@hotmail.com>
2022-04-24 18:19:58 -0300

    correccion de codigo

--- js/carrito.js
+++ js/carrito.js
@@ -1,12 +1,17 @@
-// Variables del Negocio
+// Variables 
 const monto_maximo = 100000;
 const cantidad_maxima = 100;
-const descuento = 10;
-const iva = 21;
-const interes3 = 25; // Intereses para 2, 3, 4 y 5 Cuotas
-const interes6 = 35; // Intereses para 6, 7, 8, 9, 10 y 11 Cuotas
-const interes12 = 45; // Intereses para 12 Cuotas
+const descuento = (d) => {return d * 0.10};
+const iva = (i) => {return i * 0.21};
+const suma = (a,b) => {return a + b};
+const resta = (a,b) => {return a - b};
+const multip = (a,b) => {return a * b};
+const divid = (a,b) => {return a / b};
+const interes3 = (x) => {return x * 0.25}; // Intereses para 2, 3, 4 y 5 Cuotas
+const interes6 = (x) => {return x * 0.35}; // Intereses para 6, 7, 8, 9, 10 y 11 Cuotas
+const interes12 = (x) => {return x * 0.45}; // Intereses para 12 Cuotas
 
+ 
 // Entrada de Datos
 let nombre_producto = prompt("Ingrese el Nombre del Producto");
 let precio_producto = parseFloat(prompt("Ingrese el Precio del Producto"));
@@ -32,11 +37,11 @@ if (metodo_pago == "EFECTIVO") {
 }
 
 // Calcular el Total a Pagar
-let total_pagar_bruto = precio_producto * cantidad_producto;
-let total_pagar = precio_producto * cantidad_producto;
+let total_pagar_bruto = multip ( precio_producto , cantidad_producto);
+let total_pagar = multip ( precio_producto , cantidad_producto);
 console.log("Total a Pagar: " + total_pagar_bruto);
 //Agrego el IVA al Total a Pagar
-let total_pagar_iva = total_pagar_bruto + ((total_pagar_bruto * iva) / 100);
+let total_pagar_iva = suma ( total_pagar_bruto, iva (total_pagar_bruto));
 console.log("Total a Pagar (IVA): " + total_pagar_iva);
 
 let descuento_aplicado;
@@ -44,7 +49,7 @@ let total_pagar_descuento;
 
 // Verificiar si supera el Monto Máximo o la Cantidad Máxima de Productos
 if ((total_pagar_iva >= monto_maximo) || (cantidad_producto >= cantidad_maxima)) {
-    total_pagar_descuento = total_pagar_iva - ((total_pagar_iva * descuento) / 100);
+    total_pagar_descuento = resta (total_pagar_iva, descuento(total_pagar_iva));
     total_pagar = total_pagar_descuento;
     console.log("Total a Pagar con Descuento: " + total_pagar);
     descuento_aplicado = true;
@@ -61,18 +66,18 @@ let interes;
 if ((cuotas == 0) || (cuotas == 1)) {
     interes_aplicado = false;
 } else if ((cuotas >= 2) && (cuotas <=5)) {
-    total_pagar_interes = total_pagar + ((total_pagar * interes3) / 100);
-    total_pagar_cuotas = total_pagar_interes / cuotas;
+    total_pagar_interes = suma (total_pagar , interes3(total_pagar));
+    total_pagar_cuotas = divid (total_pagar_interes, cuotas);
     interes_aplicado = true;
     interes = interes3;
 } else if ((cuotas >= 6) && (cuotas <=11)) {
-    total_pagar_interes = total_pagar + ((total_pagar * interes6) / 100);
-    total_pagar_cuotas = total_pagar_interes / cuotas;
+    total_pagar_interes = suma (total_pagar , interes6(total_pagar));
+    total_pagar_cuotas = divid (total_pagar_interes, cuotas);
     interes_aplicado = true;
     interes = interes6;
 } else {
-    total_pagar_interes = total_pagar + ((total_pagar * interes12) / 100);
-    total_pagar_cuotas = total_pagar_interes / cuotas;
+    total_pagar_interes = suma (total_pagar, interes12(total_pagar));
+    total_pagar_cuotas = divid (total_pagar_interes, cuotas);
     interes_aplicado = true;
     interes = interes12;
 }
@@ -82,17 +87,20 @@ let mensaje = "Producto: " + nombre_producto + "\n";
 mensaje += "Precio por Unidad: " + precio_producto + "\n";
 mensaje += "Cantidad de Productos: " + cantidad_producto + "\n";
 mensaje += "Total Bruto: $" + total_pagar_bruto + "\n";
-mensaje += "Total con IVA: (" + iva + "%) $" + total_pagar_iva + "\n";
+mensaje += "Total con IVA: $ " + total_pagar_iva + "\n";
 
 if (descuento_aplicado) {
-    mensaje += "Total con Descuento: (" + descuento + "%) $" + total_pagar_descuento + "\n";
+    mensaje += "Total con Descuento:  $" + total_pagar_descuento + "\n";
+    total_pagar = total_pagar_descuento;
+    
 }
 
 if (interes_aplicado) {
-    mensaje += "Total con Interés: (" + interes + "%) $" + total_pagar_interes + " \n";
+    mensaje += "Total con Interés:  $" + total_pagar_interes + " \n";
     mensaje += "Cuotas: " + cuotas + " de $" + total_pagar_cuotas + "\n";
+    total_pagar = total_pagar_cuotas;
 }
 
-mensaje += "Total a Pagar: $" + total_pagar;
+mensaje += "TOTAL A PAGAR: $" + total_pagar;
 alert(mensaje);
 console.log(mensaje);
